031c999183bebb1e3b874391d2e2d48cb504137f
Koen Van Duyse <koen@cardifftax.com>
2025-09-13 13:13:20 -0700

    Fixed  messaging

--- app/messages/[connectionId]/page.tsx
+++ app/messages/[connectionId]/page.tsx
@@ -489,7 +489,7 @@ export default function ChatThreadPage() {
                         Test Stream Config
                       </button>
                       <button
-                        onClick={createStreamChannel}
+                        onClick={() => createStreamChannel(true)}
                         className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors font-medium"
                       >
                         Create Stream Channel
